8cae353fea7b84efbfb5e3b323b4ce0c3176b41c
Emmanuel DEQUIDT <azelnoot@gmail.com>
2017-10-31 12:57:29 +0100

    autodeploy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  app:any ={nom: String, version: Number};
 
   constructor(public navCtrl: NavController) {
+    //app = new App;
+    this.app.nom = "nomApp";
+    this.app.version = 0.1;
+  }
+  //function changeName() {
+  //  this.app.nom = "nom modifié";
+  //}
+}
 
+ class App {
+constructor(public nom: String,public version: Number){
   }
 
 }
